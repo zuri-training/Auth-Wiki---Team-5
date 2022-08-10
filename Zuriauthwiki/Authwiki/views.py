430bf9d41645d4bc5a4dc90b766476d25c2e8104
Directormichaels <uzonwafor@gmail.com>
2022-08-10 07:12:28 +0100

    included urls
    added httpresponse to test

--- Zuriauthwiki/Authwiki/views.py
+++ Zuriauthwiki/Authwiki/views.py
@@ -1,3 +1,7 @@
+from ast import Return
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(response):
+    return HttpResponse('<h1>Home Page</h1>')
